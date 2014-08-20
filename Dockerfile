8a2b26a9c7dc6d59a726e197586a8bd85cdb21c0
James Carr <james.r.carr@gmail.com>
2014-08-20 16:24:42 -0500

    Dynamically generate configuration

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:14.04
 
 RUN apt-get update
-RUN apt-get install ruby1.9.1 ruby1.9.1-dev libtool make automake supervisor curl -qy
+RUN apt-get install libtool make automake supervisor curl python3-pip -qy
 
 RUN apt-get install -y memcached
 
@@ -10,14 +10,17 @@ RUN curl -qL https://twemproxy.googlecode.com/files/nutcracker-0.3.0.tar.gz | ta
 RUN cd nutcracker-0.3.0 && ./configure --enable-debug=log && make && mv src/nutcracker /twemproxy
 RUN cd / && rm -rf nutcracker-0.3.0
 
-# install nutcracker web
-RUN gem install nutcracker-web 
+# install pip deps
+RUN pip3 install pyaml
+RUN pip3 install boto
 
 # Configuration
 RUN mkdir -p /etc/nutcracker
-ADD supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-ADD config.yaml /etc/nutcracker/memcache.yaml
-
+ADD generate_configs.py /generate_configs.py
+ADD run.sh /run.sh
+RUN chmod a+x run.sh
 
 EXPOSE 3000 22222 22123
-CMD ["/usr/bin/supervisord"]
+CMD ["/run.sh"]
+
+RUN apt-get remove libtool make automake curl -yq
---
+++ generate_configs.py
@@ -0,0 +1,70 @@
+import os
+import configparser
+import boto
+import yaml
+
+
+
+def write_nutcracker_config(backends):
+    with open('/etc/nutcracker/nutcracker.yaml', 'w') as nutcracker:
+        nutcracker.write(yaml.dump({
+            'gamma':{
+                'listen': '0.0.0.0:22121',
+                'hash': 'fnv1a_64',
+                'hash_tag': 'P:',
+                'distribution': 'ketama',
+                'timeout': 250,
+                'auto_eject_hosts': False,
+                'servers': backends
+            }
+        }))
+
+def write_supervisor_config(memcached_backends=[]):
+    config = configparser.ConfigParser()
+    config['supervisord'] = {
+        'nodaemon':True,
+    }
+    config['program:nutcracker'] = {
+        'command':'/usr/local/bin/nutcracker -c /etc/nutcracker/nutcracker.yaml -s 22222 -v 6',
+    }
+
+    index = 0
+    for memcache in memcached_backends:
+        index = index + 1
+        config['program:memcached_{}'.format(index)] = {
+            'command':'/usr/bin/memcached -p {}'.format(memcache),
+            'user': 'memcache',
+        }
+
+
+    with open('/etc/supervisor/conf.d/supervisord.conf', 'w') as supervisor:
+        config.write(supervisor)
+
+def get_aws_ip(instance, use_public_ip):
+    if use_public_ip:
+        return instance.ip_address
+    else:
+        return instance.private_ip_address
+
+def generate_config_files():
+    tag_name  = os.environ.get('AWS_TAG_NAME', None)
+    tag_value = os.environ.get('AWS_TAG_VALUE', None)
+    public_ip = 'true' == os.environ.get('AWS_PUBLIC_IP', 'true').lower()
+    region    = os.environ.get('AWS_REGION', 'us-east-1')
+
+    if tag_name and tag_value:
+        conn = boto.ec2.connect_to_region(region)
+        instances = conn.get_only_instances(filters={'tag:{}'.format('tag_name'):tag_value})
+
+        backends = ['{}:11211:1'.format(get_aws_ip(instance, public_ip)) for instance in instances]
+        write_nutcracker_config(backends)
+        write_supervisor_config()
+        
+
+    else:
+        write_nutcracker_config(['127.0.0.1:11211:1', '127.0.0.1:11212:1'])
+        write_supervisor_config(['11211','11212'])
+
+
+if __name__ == '__main__':
+    generate_config_files()
